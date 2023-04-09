6ba2d5cefa570100b1b92c2be41a579b0c466b6a
aspirantzhang <admin@aspirantzhang.com>
2023-04-09 03:46:39 +0000

    feat: lesson 52

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,22 @@
+<div class=" space-x-3 bg-gray-100 rounded-xl border border-gray-200 p-4 flex">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/100" alt="">
+    </div>
+    <div class="space-y-2">
+        <header>
+            <h3 class="font-bold">Aspirant Zhang</h3>
+            <p class="text-xs">
+                Posted
+                <time>
+                    8 Months ago
+                </time>
+            </p>
+        </header>
+        <p class="text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi quis cum a minus. Nostrum
+            exercitationem odit ipsa laborum, nihil inventore et quisquam, earum aliquam modi, in facere
+            at
+            iusto sed.
+        </p>
+    </div>
+</div>
--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -4,7 +4,7 @@
             <div class="col-span-3">
                 <img src="/images/illustration-1.png" alt="" class="rounded-xl">
                 <p class="text-gray-400 text-xs mt-5 text-center">
-                    Published <time>{{ $post->created_at->diffForHumans()}}</time>
+                    Published <time>{{ $post->created_at->diffForHumans() }}</time>
                 </p>
                 <div class="flex text-sm items-center mt-3 justify-center">
                     <img src="/images/lary-avatar.svg" alt="lara-avatar">
@@ -19,8 +19,7 @@
                         <a href="/posts" class="inline-flex items-center hover:text-blue-500">
                             <svg width="22" height="22" viewBox="0 0 22 22">
                                 <g fill="none" fill-rule="evenodd">
-                                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5"
-                                        d="M21 1v20.16H.84V1z">
+                                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
                                     </path>
                                     <path class="fill-current"
                                         d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
@@ -39,6 +38,13 @@
                 <div class="space-y-4 mt-8">
                     {!! $post->body !!}
                 </div>
+            </div>
+            <div class="col-span-8 col-start-4 mt-3 space-y-4">
+                <x-post-comment />
+                <x-post-comment />
+                <x-post-comment />
+                <x-post-comment />
+                <x-post-comment />
             </div>
         </article>
     </main>
