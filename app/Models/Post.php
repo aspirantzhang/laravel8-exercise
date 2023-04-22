0cc95328e93f1001542ec2541a42b963ab97b6e6
aspirantzhang <admin@aspirantzhang.com>
2023-04-22 11:26:29 +0000

    feat: eloquent relationships 4

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -53,4 +53,9 @@ class Post extends Model
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class)->withTimestamps();
+    }
 }
