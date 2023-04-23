7c49e2df77ff1de6341691704209e24d560f4bdf
aspirantzhang <admin@aspirantzhang.com>
2023-04-23 12:57:47 +0000

    feat: eloquent relationships 4

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\UserCategory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -16,6 +17,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'category_id' => UserCategory::factory(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
